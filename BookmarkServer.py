2a579cddf241ba2166d1ce0fbc4fbd831e0fb2ae
rtelles64 <rtelles64@gmail.com>
2019-04-13 09:00:23 -0700

    Use PORT from environment

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -41,6 +41,7 @@
 #
 # After writing each step, restart the server and run test.py to test it.
 
+import os
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
@@ -149,6 +150,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))  # Use PORT if it's there (heroku)
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
